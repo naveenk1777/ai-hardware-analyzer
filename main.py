34c0bd468247f04e4b9bd9e40eeba3724896294b
naveenk1777 <naveenkkhd@gmail.com>
2026-08-19 19:02:25 +0530

    Create FastAPI app for telemetry ingestion and diagnostics
    Implement FastAPI application for IoT telemetry and diagnostics.

---
+++ main.py
@@ -0,0 +1,63 @@
+from fastapi import FastAPI,Depends
+from typing import Annotated
+from pydantic import BaseModel
+from sqlalchemy import future
+from sqlalchemy.ext.asyncio import AsyncSession
+from database import Base,engine,Asyncsessionlocal,Sensortelemetry,Devicehealth
+from contextlib import asynccontextmanager
+from sqlalchemy.future import select
+from analyzewithai import analyzewithai
+
+
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+app=FastAPI(title="iot predictive cloud bridge",lifespan=lifespan)
+
+async def get_db():
+    async with Asyncsessionlocal() as session:
+        yield session
+
+dbsession=Annotated[AsyncSession, Depends(get_db)]
+
+class telemetrypayload(BaseModel):
+    device_id:str
+    batteryper:int
+    irstus:bool
+
+@app.post("/api/telemetry")
+async def ingestdata(payload:telemetrypayload,db:dbsession):
+    new_record=Sensortelemetry(device_id=payload.device_id,batteryper=payload.batteryper,irstus=payload.irstus)
+    db.add(new_record)
+    await db.commit()
+    return ("sensor data entered in database")
+
+
+@app.post("/api/diagnose/{device_id}")
+async def run_diagnostics(device_id:str,db:dbsession):
+    query=(
+    select(Sensortelemetry)
+    .where(Sensortelemetry.device_id == device_id)
+    .order_by(Sensortelemetry.id.desc())
+    )
+    result=await db.execute(query)
+    telemetry=result.scalars().all()
+    formatted_data = [
+        {"device_id": d.device_id, "battery": d.batteryper, "irstus": d.irstus} 
+        for d in telemetry
+    ]
+    print("Sending telemetry to AI for analysis...")
+
+    ai_verdict = analyzewithai(formatted_data)
+    
+    return {
+        "ai_diagnosis": ai_verdict,
+        "data_points_analyzed": len(formatted_data),
+        "raw_data": formatted_data
+    }
+    
+
