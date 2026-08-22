b24cd9f3365b4f491a4478bef02f26090efd23dc
naveenk1777 <naveenkkhd@gmail.com>
2026-08-22 16:52:52 +0530

    Fix variable names in analyzewithai function

--- analyzewithai.py
+++ analyzewithai.py
@@ -13,8 +13,8 @@ def analyzewithai(telemetry_array:list)->AIreport:
     prompt=f"""
     You are a embeded systems diagnostic machine
     analyze the last 100 readings of {telemetry_array}
-    if irstus is consistently true flag it as sensor blocked fault
-    if battery is dropping too fast flag it as power failure
+    if ir_status is consistently true flag it as sensor blocked fault
+    if battery_percent is dropping too fast flag it as power failure
     """
     response=client.models.generate_content (
         model='gemini-2.5-flash',
