9fb92a6628b1e87d6282b9dbf5e6ef35ce8c0b2a
naveenk1777 <naveenkkhd@gmail.com>
2026-08-22 17:14:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -56,39 +56,40 @@ async def ingestdata(payload:telemetrypayload,db:dbsession):
 
 @app.post("/api/diagnose/{device_id}")
 async def run_diagnostics(device_id:str,db:dbsession):
-    query=(
-    select(Sensortelemetry)
-    .where(Sensortelemetry.device_id == device_id)
-    .order_by(Sensortelemetry.id.desc())
-    .limit(100)
-    )
-    result=await db.execute(query)
-    telemetry=result.scalars().all()
-
-    if not telemetry:
-            raise HTTPException(status_code=404, detail="No telemetry data found for this device.")
+    try:
+        query=(
+        select(Sensortelemetry)
+        .where(Sensortelemetry.device_id == device_id)
+        .order_by(Sensortelemetry.id.desc())
+        .limit(100)
+        )
+        result=await db.execute(query)
+        telemetry=result.scalars().all()
     
-    formatted_data = [
-        {"device_id": d.device_id, "battery": d.batteryper, "irstus": d.irstus} 
-        for d in telemetry
-    ]
-    print("Sending telemetry to AI for analysis...")
-
-    ai_verdict = await analyzewithai(formatted_data)
-
-    health_record = Devicehealth(
-        device_id=device_id,
-        fault_detected=ai_verdict.fault_detected,
-        fault_reason=ai_verdict.fault_reason
-    )
-    db.add(health_record)
-    await db.commit()
+        if not telemetry:
+                raise HTTPException(status_code=404, detail="No telemetry data found for this device.")
+        
+        formatted_data = [
+            {"device_id": d.device_id, "battery": d.batteryper, "irstus": d.irstus} 
+            for d in telemetry
+        ]
+        print("Sending telemetry to AI for analysis...")
     
-    return {
-        "ai_diagnosis": ai_verdict,
-        "data_points_analyzed": len(formatted_data),
-        "raw_data": formatted_data
-    }
+        ai_verdict = await analyzewithai(formatted_data)
+    
+        health_record = Devicehealth(
+            device_id=device_id,
+            fault_detected=ai_verdict.fault_detected,
+            fault_reason=ai_verdict.fault_reason
+        )
+        db.add(health_record)
+        await db.commit()
+        
+        return {
+            "ai_diagnosis": ai_verdict,
+            "data_points_analyzed": len(formatted_data),
+            "raw_data": formatted_data
+        }
 
     except Exception as e:
         print(f"Error during diagnostics: {str(e)}")
